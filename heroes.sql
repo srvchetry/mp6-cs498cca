7336ddb1658a5e9fe765403e018a1aae110a7cca
srvchetry <35620488+srvchetry@users.noreply.github.com>
2021-02-11 23:53:09 -0500

    Add files via upload

---
+++ heroes.sql
@@ -0,0 +1,38 @@
+CREATE database mp6;
+USE mp6;
+CREATE TABLE heroes (
+  id INT UNSIGNED AUTO_INCREMENT,
+  hero VARCHAR(30),
+  power VARCHER(30),  
+  name VARCHAR(30),
+  kp INT,
+  color VARCHAR(20),
+  PRIMARY KEY(id));
+
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("yes","fly","batman","100","black");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("yes","run","superman","10","red");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("no","eat","bananaman","30","yellow");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("yes","fly","spiderman","45","red");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("no","jump","goatman","33","white");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("maybe","fight","brave","45","blue");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("no","wash","cinderella","12","pink");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("no","freeze","elisa","34","white");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("yes","steal","robinhood","67","green");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("no","think","gandalf","86","grey");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("no","run","catman","84","brown");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("no","catch","dogman","47","brown");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("maybe","stink","eggman","58","white");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("maybe","swim","fishman","58","blue");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("no","fly","birdman","2","grey");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("no","fly","flyman","32","black");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("no","stay","sofaman","77","red");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("maybe","scream","beast","33","brown");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("no","hide","goblin","67","green");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("yes","smile","charming","97","white");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("yes","stink","shrek","32","green");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("no","eat","appleman","25","red");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("maybe","grow","spinachman","65","green");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("no","hide","antman","37","black");
+INSERT INTO heroes (hero,power,name,kp,color) VALUES ("yes","steal","mouseman","76","grey");
+
+
